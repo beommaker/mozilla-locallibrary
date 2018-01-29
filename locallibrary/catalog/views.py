7639abc475e7c21ed5bc15df7ebe199fcc85c1bf
beommaker <beom.maker@gmail.com>
2018-01-29 19:41:39 +0900

    feat(library): add-booklist
    https://app.asana.com/0/513674312489999/538057853929358

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 
 
 def index(request):
 
     # Generate counts of some of the main objects
-    num_books=Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
-    num_genres=Genre.objects.all().count()
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count()
     # Available books (status = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # The 'all()' is implied by default.
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
 
     # Render the HTML template index.html with the data in the context variable
     return render(
@@ -23,3 +24,14 @@ def index(request):
             'num_authors':num_authors,
             'num_genres':num_genres}
     )
+
+
+class BookListView(generic.ListView):
+
+    model = Book
+    # your own name for the list as a template variable
+    context_object_name = 'my_book_list'
+    # Get 5 books containing the title 'life'
+    queryset = Book.objects.filter(title__icontains='Life'[:5])
+    # Specify your own template name/location
+    template_name = 'book_list.html'
