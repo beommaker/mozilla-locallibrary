c08ca95af7800537361202b56e1960c8044272eb
beommaker <beom.maker@gmail.com>
2018-01-29 18:19:47 +0900

    feat(library): add-catalog-admin
    https://app.asana.com/0/513674312489999/538057853929358

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -20,14 +20,20 @@ class Book(models.Model):
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book.")
 
-    def __str__(self):
 
+    def __str__(self):
+    
         return self.title
 
     def get_absolute_url(self):
-
+    
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+        
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+    
+    display_genre.short_description = 'Genre'
 
 class BookInstance(models.Model):
 
@@ -71,4 +77,4 @@ class Author(models.Model):
 
     def __str__(self):
 
-        return '{0}, {1}'.format(self.last_namem, self.first_name)
+        return '{0}, {1}'.format(self.last_name, self.first_name)
