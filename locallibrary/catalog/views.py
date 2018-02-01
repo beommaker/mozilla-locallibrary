0d095c156d81f676e5ec04ffb08905ed8f3b6681
beommaker <beom.maker@gmail.com>
2018-02-01 18:40:35 +0900

    feat(library): add-author-form
    https://app.asana.com/0/513674312489999/538057853929358

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
@@ -119,3 +120,22 @@ def renew_book_librarian(request, pk):
         form = RenewBookForm(initial={'renewal_date': proposed_renewal_date, })
 
     return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst': book_inst})
+
+
+class AuthorCreate(CreateView):
+ 
+    model = Author
+    fields = '__all__'
+    # initial = {'date_of_death': '05/01/2018', }
+
+
+class AuthorUpdate(UpdateView):
+
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+class AuthorDelete(DeleteView):
+ 
+    model = Author
+    success_url = reverse_lazy('authors')
